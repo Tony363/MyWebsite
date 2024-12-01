189c8e185d54e3bf39998df3f5e966bbc4b4ccdb
Tony <thefool363@gmail.com>
2024-11-30 19:56:40 -0500

    can contintue to update

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Multi-Modal Generative AI", "Computer Vision", "Natural Language Processing", "Data Engineering", "Statistical Optimization"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
