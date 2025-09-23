a9018bed1c131f687116970396f001b0d755294e
Tony <thefool363@gmail.com>
2025-09-23 14:53:09 -0700

    color contrasts

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#ffffff"
       },
       "shape": {
         "type": "circle",
@@ -41,7 +41,7 @@ particlesJS('particles-js',
         }
       },
       "opacity": {
-        "value": 0.5,
+        "value": 0.3,
         "random": false,
         "anim": {
           "enable": false,
@@ -63,8 +63,8 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000000",
-        "opacity": 0.4,
+        "color": "#ffffff",
+        "opacity": 0.2,
         "width": 1
       },
       "move": {
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#000000",
+      "background_color": "transparent",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
